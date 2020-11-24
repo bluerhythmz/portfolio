201e1864ad1eaca77d71173f2f28df202dd00f05
bluerhythmz <73447805+bluerhythmz@users.noreply.github.com>
2020-11-24 13:21:03 -0800

    Add files via upload

---
+++ portfolio/main.js
@@ -0,0 +1,42 @@
+let startBtn = document.getElementById('start');
+let intro = document.querySelector('.intro');
+let logo = document.getElementById('logo');
+let mainPage = document.querySelector('.primary')
+let aboutBtn = document.getElementById('about-btn')
+let about = document.querySelector('.about-container')
+let projectBtn = document.getElementById('project-btn')
+let projects = document.querySelector('.project-container')
+let contactBtn = document.getElementById('contact-btn')
+let contact = document.querySelector('.contact-container')
+
+startBtn.addEventListener('click', function() {
+    intro.style.opacity = "0";
+    intro.style.zIndex = "0";
+    logo.style.bottom = "600px"
+    mainPage.style.display = "block"
+    mainPage.style.opacity = "1"
+})
+
+projectBtn.addEventListener('click', () => {
+    projects.classList.add('active')
+    about.classList.add('hide')
+    about.classList.remove('active')
+    contact.classList.add('hide')
+    contact.classList.remove('active')
+})
+
+aboutBtn.addEventListener('click', () => {
+    projects.classList.add('hide')
+    about.classList.add('active')
+    projects.classList.remove('active')
+    contact.classList.add('hide')
+    contact.classList.remove('active')
+})
+
+contactBtn.addEventListener('click', () => {
+    projects.classList.add('hide')
+    about.classList.add('hide')
+    projects.classList.remove('active')
+    about.classList.remove('active')
+    contact.classList.add('active')
+})
